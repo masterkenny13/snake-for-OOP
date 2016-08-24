63a1d195764f3fd2e6f6ddd878dd32cf4790b789
masterkenny13 <kopachroman@gmail.com>
2016-08-24 22:42:39 +0300

    first play with functions

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,25 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("my first line");
+            int x1 = 2;
+            int y1 = 5;
+            char sym1 = '@';
+
+            Drow(x1, y1, sym1);
+
+
+            int x2 = 5;
+            int y2 = 7;
+            char sym2 = '*';
+
+            Drow(x2, y2, sym2);
+
             Console.ReadLine();
+        }
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
